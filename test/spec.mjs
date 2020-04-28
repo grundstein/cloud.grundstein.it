806f0e427a198d0d2a6b743c31fee5cfdae12a33
jascha <jascha@jaeh.at>
2020-04-28 18:31:58 +0200

    test: update spec

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,15 +1,11 @@
 import { is } from '@magic/test'
 
-import hosts,{ gps, grs, gul, ghs, gbs, repos } from '../src/index.mjs'
+import config from '../grundstein-config.mjs'
+
+console.log(config)
 
 export default [
-  { fn: is.object(hosts), info: 'hosts is an object' },
-  { fn: is.array(hosts.repos), info: 'host.repos is an array' },
-  { fn: is.deep.equal(hosts.gps, gps), info: 'host.gps and gps are equal' },
-  { fn: is.deep.equal(hosts.grs, grs), info: 'host.grs and grs are equal' },
-  { fn: is.deep.equal(hosts.gul, gul), info: 'host.gul and gul are equal' },
-  { fn: is.deep.equal(hosts.ghs, ghs), info: 'host.ghs and ghs are equal' },
-  { fn: is.deep.equal(hosts.gbs, gbs), info: 'host.gbs and gbs are equal' },
-  { fn: is.deep.equal(hosts.repos, repos), info: 'host.repos and repos are equal' },
-  { fn: is.empty(repos), expect: false, info: 'repos is not empty' },
+  { fn: is.object(config), info: 'config is an object' },
+  { fn: is.object(config.env), info: 'config.env is an object' },
+  { fn: is.array(config.hosts), info: 'config.hosts is an array' },
 ]
