cd2aa012ac0588223efb99d6d5b1073df0ffb2d8
jascha <jascha@jaeh.at>
2020-04-28 22:57:04 +0200

    grundstein-config.mjs: update ip and hostnames

--- grundstein-config.mjs
+++ grundstein-config.mjs
@@ -3,12 +3,12 @@ import { env, gas, gps, grs, gms } from 'grundstein/src/defaultConfig.mjs'
 export const hosts = [
   {
     // the (internal!) root dns identifier of this pod
-    name: 'grundstein.it',
-    fqdn: 'cloud.grundstein.it',
+    name: 'wiznwit.com',
+    fqdn: 'cloud.wiznwit.com',
 
     // which ip addresses this server is reachable at. used by ssh to connect to the server.
     // this option will disappear once pods get provisioned through the api.
-    ip: '178.128.136.72',
+    ip: '157.245.23.135',
 
     // the hostnames this pod will serve publicly,
     // this is in addition to the hosts in the repositories list below
