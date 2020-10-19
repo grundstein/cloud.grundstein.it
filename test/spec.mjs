f9e1f9638f141a037fa45d4909e8183cbcc1dfb8
jascha <jascha@jaeh.at>
2020-10-20 00:50:54 +0200

    test: remove console

--- test/spec.mjs
+++ test/spec.mjs
@@ -2,8 +2,6 @@ import { is } from '@magic/test'
 
 import config from '../grundstein-config.mjs'
 
-console.log(config)
-
 export default [
   { fn: is.object(config), info: 'config is an object' },
   { fn: is.object(config.env), info: 'config.env is an object' },
